02f5ec9f9dfe160b41e59d9e6c1bcd5238b455d5
Sandro Turriate <sandro.turriate@gmail.com>
2009-12-04 23:47:03 -0500

    Add SystemInformation module

---
+++ lib/bench_press/system_information.rb
@@ -0,0 +1,82 @@
+module BenchPress
+  module SystemInformation
+    require 'digest/sha1'
+    extend self
+
+    def announce_cpu
+      [
+        announce("CPU", cpu),
+        announce("Processor Count", processor_count)
+      ].join("\n")
+    end
+
+    def announce_memory
+      announce "Memory", memory
+    end
+
+    def announce_os
+      announce "Operating System", os
+    end
+
+    def announce_ruby_version
+      announce "Ruby version", ruby_version
+    end
+
+    def cpu
+      if mac?
+        "#{facts['sp_cpu_type']} #{facts['sp_current_processor_speed']}"
+      else
+        facts['processor0']
+      end
+    end
+
+    def crypted_identifier
+      Digest::SHA1.hexdigest(identifier)
+    end
+
+    def memory
+      facts['sp_physical_memory'] || facts['memorysize']
+    end
+
+    def os
+      facts['sp_os_version'] || facts['lsbdistdescription'] || facts['operatingsystem']
+    end
+
+    def processor_count
+      facts['sp_number_processors'] || facts['processorcount'] || facts['physicalprocessorcount']
+    end
+
+    def ruby_version
+      "#{RUBY_VERSION} patchlevel #{RUBY_PATCHLEVEL}"
+    end
+
+    def summary
+      [
+        announce_os,
+        announce_cpu,
+        announce_memory,
+        announce_ruby_version
+      ].join("\n")
+    end
+
+    protected
+
+    def announce(key, value)
+      key.ljust(19) << value
+    end
+
+    def facts
+      @facts ||= begin
+        Facter.to_hash
+      end
+    end
+
+    def identifier
+      facts['sp_serial_number'] || facts['uniqueid']
+    end
+
+    def mac?
+      Facter.kernel =~ /Darwin/
+    end
+  end
+end
---
+++ spec/bench_press/system_information_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe BenchPress::SystemInformation do
+  before do
+    BenchPress::SystemInformation.stub(:ruby_version => "1.8.7 patchlevel 174")
+  end
+
+  context "for Mac OS X" do
+    before do
+      @facts = {
+        'sp_physical_memory' => '4 GB',
+        'sp_os_version' => 'Mac OS X 10.6.2 (10C540)',
+        'sp_cpu_type' => 'Intel Core 2 Duo',
+        'sp_current_processor_speed' => '2.4 GHz',
+        'sp_number_processors' => '2',
+        'sp_serial_number' => '123ABC'
+      }
+      BenchPress::SystemInformation.stub(:mac? => true)
+      BenchPress::SystemInformation.stub(:facts => @facts)
+    end
+
+    it "summarizes system information" do
+      summary = <<-EOS
+Operating System   Mac OS X 10.6.2 (10C540)
+CPU                Intel Core 2 Duo 2.4 GHz
+Processor Count    2
+Memory             4 GB
+Ruby version       1.8.7 patchlevel 174
+      EOS
+      BenchPress::SystemInformation.summary.should == summary.strip
+    end
+
+    it "encrypts the serial number" do
+      BenchPress::SystemInformation.crypted_identifier.should == Digest::SHA1.hexdigest("123ABC")
+    end
+  end
+
+  context "for Linux" do
+    before do
+      @facts = {
+        'memorysize' => '254.75 MB',
+        'lsbdistdescription' => 'Ubuntu 8.10',
+        'processor0' => 'Dual Core AMD Opteron(tm) Processor 270',
+        'processorcount' => '4',
+        'uniqueid' => '123ABC'
+      }
+      BenchPress::SystemInformation.stub(:mac? => false)
+      BenchPress::SystemInformation.stub(:facts => @facts)
+    end
+
+    it "summarizes system information" do
+      summary = <<-EOS
+Operating System   Ubuntu 8.10
+CPU                Dual Core AMD Opteron(tm) Processor 270
+Processor Count    4
+Memory             254.75 MB
+Ruby version       1.8.7 patchlevel 174
+      EOS
+      BenchPress::SystemInformation.summary.should == summary.strip
+    end
+
+    it "encrypts the unique id" do
+      BenchPress::SystemInformation.crypted_identifier.should == Digest::SHA1.hexdigest("123ABC")
+    end
+  end
+end
