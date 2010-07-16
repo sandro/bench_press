fb1120182a999202fe88dc9457e1dfd97f614b14
Sandro Turriate <sandro.turriate@gmail.com>
2010-07-16 10:47:41 -0400

    Tests for publishing benchmark

--- lib/bench_press/ruby_benchmark.rb
+++ lib/bench_press/ruby_benchmark.rb
@@ -1,14 +1,31 @@
 module BenchPress
   require 'httparty'
+
   class RubyBenchmark
     include HTTParty
     base_uri 'http://rubybenchmark.com'
 
-    def self.publish(pressed, file_path)
-      report = pressed.report.to_hash
+    def self.publish(measurable, file_path)
+      report = measurable.report.to_hash
       report[:results_attributes] = report.delete(:runnables)
       report.merge! :source => File.read(file_path)
-      post("/reports", :query => {:report => report})
+      new post("/reports", :query => {:report => report})
+    end
+
+    attr_reader :response
+
+    def initialize(response)
+      @response = response
+    end
+
+    def report_url
+      response.body if successful?
+    end
+
+    protected
+
+    def successful?
+      Net::HTTPSuccess === response.response
     end
   end
 end
---
+++ spec/bench_press/ruby_benchmark_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe BenchPress::RubyBenchmark do
+  let(:measurable) { Measurable.dup }
+
+  before do
+    BenchPress::RubyBenchmark.base_uri "http://localhost:3000"
+  end
+
+  describe "#report_url" do
+    subject do
+      BenchPress::RubyBenchmark.publish measurable, measurable.path
+    end
+
+    context "when report is successfully created" do
+      before do
+        measurable.bench_press
+        Benchmark.stub(:realtime => 1)
+      end
+
+      it "returns the response body" do
+        subject.report_url.should =~ %r(http://localhost:3000/reports/\d+)
+      end
+    end
+
+    context "when creating the report is unsuccessful" do
+      before do
+        invalid_hash = {:runnables => {}}
+        measurable.report.stub(:to_hash => invalid_hash)
+      end
+
+      it "returns nil" do
+        subject.report_url.should be_nil
+      end
+    end
+  end
+end
