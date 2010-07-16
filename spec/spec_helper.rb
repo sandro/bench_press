f38c59cc316d2ecb7cc63201549edd2b89f14a5d
Sandro Turriate <sandro.turriate@gmail.com>
2010-07-16 10:47:00 -0400

    Explicitly control ephemeral response's expiration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,10 @@ require 'ephemeral_response'
 
 Dir.glob("spec/support/*.rb") {|f| require f}
 
+EphemeralResponse::Configuration.expiration = lambda do
+  one_day
+end
+
 Spec::Runner.configure do |config|
   config.before(:suite) do
     EphemeralResponse.activate
