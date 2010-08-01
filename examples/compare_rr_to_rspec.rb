7e8bcea7f9fe87f03e5a03934fe7a7dc9fbfc15d
Sandro Turriate <sandro.turriate@gmail.com>
2010-07-31 23:27:50 -0400

    More info in rr example

--- examples/compare_rr_to_rspec.rb
+++ examples/compare_rr_to_rspec.rb
@@ -7,6 +7,12 @@ require 'bench_press'
 module CompareRRToRspec
   extend BenchPress
 
+  name 'Compare RR mocking to Rspec'
+  author 'Sandro Turriate'
+  summary "Ported from the [benchmarks](http://github.com/btakita/rr/tree/master/benchmarks/) directory within the [rr](http://github.com/btakita/rr) project. Repetition really matters in this example as Rspec is much faster sub 100 repetitions."
+  date '2009-08-03'
+
+  reps 1200
   rspec_object = Object.new
   rr_object = Object.new
 
@@ -14,6 +20,7 @@ module CompareRRToRspec
     rspec_object.should_receive(:foobar).and_return("baz")
     rspec_object.foobar
   end
+
   measure("rr mock") do
     RR.mock(rr_object).foobar.returns("baz")
     rr_object.foobar
