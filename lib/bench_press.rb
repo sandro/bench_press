84f26627ec4f058a3d5e21c07395683f55f61ccb
Sandro Turriate <sandro.turriate@gmail.com>
2010-08-05 21:15:15 -0400

    Version bump to 0.3.0

--- lib/bench_press.rb
+++ lib/bench_press.rb
@@ -9,7 +9,7 @@ require 'bench_press/report'
 require 'bench_press/system_information'
 
 module BenchPress
-  VERSION = '0.2.1'
+  VERSION = '0.3.0'
 
   autoload :RubyBenchmark, 'bench_press/ruby_benchmark'
 
